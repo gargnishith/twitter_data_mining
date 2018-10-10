137e72d16961888a1aba7e3d5747c147730fd323
garg99 <35557113+gargnishith@users.noreply.github.com>
2018-10-10 22:53:47 +0530

    Update update.py

--- update.py
+++ update.py
@@ -1,3 +1,21 @@
 """ we can print the previous code in form of a JSON
 The JSON response from the Twitter API is available in the attribute _json (with a leading underscore)
 which is not the raw JSON string, but a dictionary."""
+
+def process_or_store(tweet):
+    print(json.dumps(tweet))
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+  process_or_store(status._json)
+
+""" if we want to have a list of all our followers """
+for friend in tweepy.Cursor(api.friends).items():
+  process_or_store(friend._json)
+  
+""" if we want a list of all our tweets """ 
+for tweet in tweepy.Cursor(api.user_timeline).items():
+    process_or_store(tweet._json)
+    
+    
+""" In this way we can easily collect tweets (and more) and store them in the original JSON format, 
+fairly easy to convert into different data models depending on our storage """
