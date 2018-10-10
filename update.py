eccfb145da8ff008087ffe34bdc53785f8f61c3e
garg99 <35557113+gargnishith@users.noreply.github.com>
2018-10-10 22:48:42 +0530

    Create update.py

---
+++ update.py
@@ -0,0 +1,3 @@
+""" we can print the previous code in form of a JSON
+The JSON response from the Twitter API is available in the attribute _json (with a leading underscore)
+which is not the raw JSON string, but a dictionary."""
